9547fcf28aeb28c077198880f41c2955789204f8
enesaslanhan <you@example.com>
2024-02-16 15:39:03 +0300

    16 şubat 1

--- src/app/admin/components/products/products.module.ts
+++ src/app/admin/components/products/products.module.ts
@@ -2,18 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductsComponent } from './products.component';
 import { RouterModule } from '@angular/router';
-
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { CreateComponent } from './create/create.component';
+import { ListComponent } from './list/list.component';
 
 
 @NgModule({
   declarations: [
-    ProductsComponent
+    ProductsComponent,
+    CreateComponent,
+    ListComponent
   ],
   imports: [
     CommonModule,
     RouterModule.forChild([
       {path:"",component:ProductsComponent}
-    ])
+    ]),
+    MatSidenavModule
+
   ]
 })
 export class ProductsModule { }
