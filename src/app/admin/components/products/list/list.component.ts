0161f05b01e2756548efbfea7cff806da81abea8
enesaslanhan <you@example.com>
2024-02-26 10:58:37 +0300

    Product-service-read

--- src/app/admin/components/products/list/list.component.ts
+++ src/app/admin/components/products/list/list.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { NgxSpinner, NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
+import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
+import { List_Product } from 'src/app/contracts/list_product';
+import { AlertifyService } from 'src/app/services/admin/alertify.service';
+import { ProductService } from 'src/app/services/common/product.service';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss']
 })
-export class ListComponent {
+export class ListComponent extends BaseComponent implements OnInit {
+  constructor(private prodcutService:ProductService,spinner:NgxSpinnerService,private alertifyService:AlertifyService,private toastrService:ToastrService){
+    super(spinner)
+  }
+  displayedColumns: string[] = ['name', 'stock', 'price', 'createdDate','updateDate'];
+  dataSource  : MatTableDataSource<List_Product>=null;
+
+  async ngOnInit() {
+    this.showSpinner(SpinnerType.BallAtom)
+    const allProducts:List_Product[]=await this.prodcutService.read(()=>this.hideSpinner(SpinnerType.BallAtom),error=>this.toastrService.error("Ürün listelenmedi"))
+    this.dataSource=new MatTableDataSource<List_Product>(allProducts)
+  }
 
 }
--- src/app/services/common/product.service.ts
+++ src/app/services/common/product.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClientService } from './http-client.service';
 import { Product } from 'src/app/contracts/product';
 import { HttpErrorResponse } from '@angular/common/http';
+import { List_Product } from 'src/app/contracts/list_product';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class ProductService {
 
   constructor(private httpClientService:HttpClientService) { }
 
-  createProduct(product:Product,successCallBack?:any,errorCallBack?:any){
+  createProduct(product:Product,successCallBack?:()=>void,errorCallBack?:(errorMessage:string)=>void){
       this.httpClientService.post({
         controller:"products"
       },product).subscribe(result=>{
@@ -26,5 +27,13 @@ export class ProductService {
         })
         errorCallBack(message)
       })
+  }
+  async read(successCallBack?:()=>void,errorCallBack?:(errorMessage:string)=>void):Promise<List_Product[]>{
+    const promiseData:Promise<List_Product[]>=this.httpClientService.get<List_Product[]>({
+      controller:"products"
+    }).toPromise();
+    promiseData.then(d=>successCallBack()).
+      catch((errorResponse:HttpErrorResponse)=>errorCallBack(errorResponse.message))
+    return await promiseData;
   }
 }
